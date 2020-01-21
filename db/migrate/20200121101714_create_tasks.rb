ab1da486c9ee3ba59cdc7335c0c82d94735eaa95
craig <craigya89@gmail.com>
2020-01-21 18:22:36 +0800

    Step.6 Generate Task model - 新增 User/ Task/ Tag model - 建立 User/ Task/ Tag table

---
+++ db/migrate/20200121101714_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :content
+      t.integer :status
+      t.integer :priority
+      t.datetime :start_at
+      t.datetime :end_at
+
+      t.timestamps
+    end
+  end
+end
