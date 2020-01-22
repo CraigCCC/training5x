67691c44d71b46351e82abc97cefba07f325b9b3
craig <craigya89@gmail.com>
2020-01-22 10:50:29 +0800

    Step7-1-2 實做 controller 和 view 必要的部份 - routes 設定 task 的 resources - tasks Controller 完成基本CRUD功能 - Task Model 新增 status 和 priority 的enum - task Views 新增 index, new, edit, show 頁面基本呈現

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,53 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    if @task.save
+      redirect_to tasks_path, notice: "新增成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(params_task)
+      redirect_to tasks_path, notice: "編輯成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "刪除成功"
+  end
+
+  private
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def params_task
+    params.require(:task).permit(:title,
+                                 :content,
+                                 :status,
+                                 :priority,
+                                 :start_at,
+                                 :end_at)
+  end
 end
