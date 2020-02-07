366d459f1ab8ab67466ef9119c4e540d0c105b10
craig <craigya89@gmail.com>
2020-02-07 13:30:42 +0800

    Step 13. Deploy on Heroku and modify basic auth - Add file "local_env.yml" to config folder and add key/value. - modify config/application.rb to load local_env

--- config.ru
+++ config.ru
@@ -3,3 +3,7 @@
 require_relative 'config/environment'
 
 run Rails.application
+
+use Rack::Auth::Basic do |username, password|
+  username == ENV['basic_account'] && password == ENV['basic_password']
+end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,15 @@ module Training5x
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Setting my local_ENV
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
   end
 end
