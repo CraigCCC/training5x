33a3858f6221f4b5e15be8362e472a42770565f3
craig <craigya89@gmail.com>
2020-03-02 20:09:41 +0800

    Fix code - Rename 'sortable' => 'sortable_link_to'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def sortable(column)
+  def sortable_link_to(column)
     direction = params[:direction] == 'DESC' ? 'ASC' : 'DESC'
     link_to t("tasks.#{column}"), order_by: column.split('.')[0], direction: direction
   end
